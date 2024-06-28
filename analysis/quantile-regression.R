8a030a86459162a70d7af03f4f629d78eab58139
sergiozxy <zxuyuan@umich.edu>
2024-06-28 18:55:10 -0400

    modified:   analysis/quantile-regression.R 	modified:   manuscript/main.aux 	modified:   manuscript/main.fdb_latexmk 	modified:   manuscript/main.fls 	modified:   manuscript/main.log 	modified:   manuscript/main.out 	modified:   manuscript/main.pdf 	modified:   manuscript/main.synctex.gz 	modified:   manuscript/main.tex 	modified:   network_estimation/network_estimate.ipynb 	modified:   network_estimation/network_estimation.ipynb 	new file:   network_estimation/network_structure.ipynb

--- analysis/quantile-regression.R
+++ analysis/quantile-regression.R
@@ -1,3 +1,4 @@
+library(qte)
 library(lfe)
 library(haven)
 library(dplyr)
@@ -5,11 +6,18 @@ library(quantreg)
 getwd()
 
 setwd('E:/umich/RealEstateBrokerage-main')
-data <- read_dta("individual.dta")
+data <- read_dta("template.dta")
 
+# so the main idea is to generate the treatment effect where the treated group 
+# happens when the
 
-data <- data %>%
-  mutate(across(matches("^yearx"), ~ . * influence, .names = "influence_{col}"))
+# our result is not influenced by the COVID because the network effect is consistent in terms of the 
+# because the stores are faced with the short term shock and the operation should be long term that they can not directly close or open new stores
+# so the network effect is consistent in terms of the COVID-19
+# Besides, we can also estimate the model trying to see the parallel trend is satisfied or not in this model
+
+# data <- data %>%
+#   mutate(across(matches("^yearx"), ~ . * influence, .names = "influence_{col}"))
 
 # now we are working on the CIC estimation
 pre_policy_data <- data[data$year %in% c(2016, 2017), ]
@@ -23,36 +31,33 @@ region_control <- c("pm25", "pop", "light")
 brokerage_control <- c("non_online_effect", "ln_nego_changes", "ln_negotiation_period")
 control_variables <- c(brokerage_control, hedonic_control, transaction_control, region_control)
 
-quantiles <- c(0.1, 0.25, 0.5, 0.75, 0.9)
-coefficients <- list()
-standard_errors <- list()
-
-# Loop over each quantile
-for (q in quantiles) {
-    # Run quantile regression
-    model <- rq(price_concession ~ policy + influence + policy_influence + control_variables, 
-                data = data, tau = q)
-    
-    # Store the coefficients and standard errors
-    coefficients[[as.character(q)]] <- coef(summary(model))["policy_influence", "Value"]
-    standard_errors[[as.character(q)]] <- coef(summary(model))["policy_influence", "Std. Error"]
-}
+quantiles <- c(0.05, 0.95, 0.05)
 
 
 dependent_variable <- c("yearx2_density", "yearx3_density", "yearx4_density", "yearx5_density", "yearx6_density", "yearx7_density", "broker_410", "ln_end_price", "ln_watch_people", "ln_watch_time")
 
-
-all_vars <- c("ln_income", dependent_variable, control_variables, "year", "bs_code", "id")
-data_subset <- data[complete.cases(data[all_vars]), ]
-# regression on the dependent variables to generate the residual
-model_felm <- felm(as.formula(paste("ln_income ~", paste(dependent_variable, collapse = " + "), "+", paste(control_variables, collapse = " + "), "| year + bs_code + id")), data = data_subset)
-data_subset$resid <- residuals(model_felm)
-
-
-
 # finally we need to estimate the counterfactual changes in the result
 
 
+c1 <- CiC(influence ~ treat, t=2018, tmin1=2016, tname="year",
+          xformla=~age + I(age^2) + education + black + hispanic + married + nodegree,
+          data=lalonde.psid.panel, idname="id", se=TRUE,
+          probs=seq(0.05, 0.95, 0.05))
 
 
-
+# I am using the qte package available in the Rstudio.
+# 
+# # Load the required library
+# library(qte)
+# 
+# data(lalonde)
+# ## Run the Change in Changes model conditioning on age, education,
+# ## black, hispanic, married, and nodegree
+# c1 <- CiC(re ~ treat, t=1978, tmin1=1975, tname="year",
+#           xformla=~age + I(age^2) + education + black + hispanic + married + nodegree,
+#           data=lalonde.psid.panel, idname="id", se=TRUE,
+#           probs=seq(0.05, 0.95, 0.05))
+# summary(c1)
+# 
+# 
+# 
